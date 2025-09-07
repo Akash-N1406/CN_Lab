017c68a6b0bdb9c5ec80a957375bb26cdc9768a3
Akash-N1406 <ak54ash5@gmail.com>
2025-09-07 15:47:26 +0530

    Update server.c

--- chat/server.c
+++ chat/server.c
@@ -3,114 +3,95 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <pthread.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 
-#define MAX_CLIENTS 10
+#define PORT 8080
 #define BUFFER_SIZE 1024
+#define MAX_CLIENTS 10
 
 int client_sockets[MAX_CLIENTS];
-pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+int client_count = 0;
 
-void broadcast(char *message, int sender_fd)
-{
-    pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < MAX_CLIENTS; i++)
-    {
-        if (client_sockets[i] != 0 && client_sockets[i] != sender_fd)
-        {
+void broadcast_message(int sender_fd, char *message) {
+    for (int i = 0; i < client_count; i++) {
+        if (client_sockets[i] != sender_fd) {
             send(client_sockets[i], message, strlen(message), 0);
         }
     }
-    pthread_mutex_unlock(&clients_mutex);
 }
 
-void *handle_client(void *arg)
-{
-    int client_fd = *((int *)arg);
+void handle_client(int client_fd) {
     char buffer[BUFFER_SIZE];
-    int bytes_read;
+    int n;
 
-    while ((bytes_read = recv(client_fd, buffer, sizeof(buffer) - 1, 0)) > 0)
-    {
-        buffer[bytes_read] = '\0';
-        printf("Client %d: %s", client_fd, buffer);
-        broadcast(buffer, client_fd);
+    while ((n = recv(client_fd, buffer, sizeof(buffer) - 1, 0)) > 0) {
+        buffer[n] = '\0';
+        printf("Message from client: %s\n", buffer);
+        broadcast_message(client_fd, buffer);
     }
 
-    // client disconnected
-    close(client_fd);
-    pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < MAX_CLIENTS; i++)
-    {
-        if (client_sockets[i] == client_fd)
-        {
-            client_sockets[i] = 0;
+    // remove client from list
+    for (int i = 0; i < client_count; i++) {
+        if (client_sockets[i] == client_fd) {
+            for (int j = i; j < client_count - 1; j++) {
+                client_sockets[j] = client_sockets[j + 1];
+            }
+            client_count--;
             break;
         }
     }
-    pthread_mutex_unlock(&clients_mutex);
 
-    return NULL;
+    close(client_fd);
+    exit(0);
 }
 
-int main()
-{
+int main() {
     int server_fd, client_fd;
     struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_size = sizeof(client_addr);
-    pthread_t tid;
+    socklen_t addr_len = sizeof(client_addr);
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1)
-    {
+    if (server_fd < 0) {
         perror("Socket creation failed");
         exit(1);
     }
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
+    server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
-    {
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Bind failed");
         close(server_fd);
         exit(1);
     }
 
-    if (listen(server_fd, 5) == -1)
-    {
+    if (listen(server_fd, 5) < 0) {
         perror("Listen failed");
         close(server_fd);
         exit(1);
     }
 
-    printf("Server listening on port 8080...\n");
+    printf("Chat server listening on port %d...\n", PORT);
 
-    while ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_size)))
-    {
-        pthread_mutex_lock(&clients_mutex);
-        int i;
-        for (i = 0; i < MAX_CLIENTS; i++)
-        {
-            if (client_sockets[i] == 0)
-            {
-                client_sockets[i] = client_fd;
-                break;
-            }
+    while (1) {
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
+        if (client_fd < 0) {
+            perror("Accept failed");
+            continue;
         }
-        pthread_mutex_unlock(&clients_mutex);
 
-        if (i == MAX_CLIENTS)
-        {
-            printf("Max clients reached. Connection rejected.\n");
-            close(client_fd);
+        if (client_count < MAX_CLIENTS) {
+            client_sockets[client_count++] = client_fd;
         }
-        else
-        {
-            pthread_create(&tid, NULL, handle_client, &client_fd);
-            pthread_detach(tid);
-            printf("Client connected.\n");
+
+        if (fork() == 0) {  // child process
+            close(server_fd); // child doesn’t need listening socket
+            handle_client(client_fd);
+        } else {
+            close(client_fd); // parent doesn’t need connected socket
         }
     }
 
