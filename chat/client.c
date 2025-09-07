78707ea3f201e8a886ae932c7e50a92c18c99204
Akash-N1406 <ak54ash5@gmail.com>
2025-09-07 15:48:01 +0530

    Update client.c

--- chat/client.c
+++ chat/client.c
@@ -3,56 +3,54 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <pthread.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 
+#define PORT 8080
 #define BUFFER_SIZE 1024
 
-int sock;
-
-void *receive_messages(void *arg)
-{
-    char buffer[BUFFER_SIZE];
-    int bytes_read;
-    while ((bytes_read = recv(sock, buffer, sizeof(buffer) - 1, 0)) > 0)
-    {
-        buffer[bytes_read] = '\0';
-        printf("\n%s", buffer);
-        fflush(stdout);
-    }
-    return NULL;
-}
-
-int main()
-{
+int main() {
+    int sock;
     struct sockaddr_in server_addr;
-    pthread_t recv_thread;
     char buffer[BUFFER_SIZE];
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1)
-    {
+    if (sock < 0) {
         perror("Socket creation failed");
         exit(1);
     }
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
+    server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // localhost
 
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
-    {
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Connection failed");
         close(sock);
         exit(1);
     }
 
-    printf("Connected to chat server.\n");
-    pthread_create(&recv_thread, NULL, receive_messages, NULL);
+    printf("Connected to chat server. Type messages...\n");
 
-    while (1)
-    {
-        fgets(buffer, BUFFER_SIZE, stdin);
-        send(sock, buffer, strlen(buffer), 0);
+    if (fork() == 0) {  
+        // Child process for receiving messages
+        while (1) {
+            int n = recv(sock, buffer, sizeof(buffer) - 1, 0);
+            if (n <= 0) {
+                printf("Disconnected from server.\n");
+                close(sock);
+                exit(0);
+            }
+            buffer[n] = '\0';
+            printf("\nMessage: %s\n", buffer);
+        }
+    } else {  
+        // Parent process for sending messages
+        while (1) {
+            fgets(buffer, BUFFER_SIZE, stdin);
+            send(sock, buffer, strlen(buffer), 0);
+        }
     }
 
     close(sock);
